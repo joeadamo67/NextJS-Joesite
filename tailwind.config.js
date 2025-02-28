6c4f61b5a053130be53be00e61b647eb48e682f6
Joe A <140087801+joeadamo67@users.noreply.github.com>
2025-02-28 17:42:47 -0600

    worky

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,16 +14,7 @@ module.exports = {
         accent: '#FFDE7D',
         foreground: '#333333',
       },
-      borderRadius: {
-        'xl': '1rem',
-        '2xl': '1.5rem',
-        '3xl': '2rem',
-      },
-      boxShadow: {
-        'custom': '0 10px 25px -5px rgba(0, 0, 0, 0.1), 0 8px 10px -6px rgba(0, 0, 0, 0.1)',
-        'custom-lg': '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)',
-      },
     },
   },
   plugins: [],
-};
+}
